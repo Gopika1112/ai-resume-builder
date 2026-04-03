ae6c27e504475f86fb02034bd9b58d2d87b75215
Gopika <gopikatoshivoham@gmail.com>
2026-04-03 18:59:06 +0530

    Final System Stability: 32KB Header Boost and Local Address Sync

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ export const getURL = (path: string = "") => {
   let url =
     process.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your production domain
     process.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set on Vercel
-    "http://localhost:3005/";
+    "http://127.0.0.1:3005/";
 
   // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`;
